f098f0efb0f9c995d22208072c41911ba55ecdfd
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-04-20 12:34:00 +0500

    homework singletone

---
+++ 6.1Homework/6.1Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _6._1Homework;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var newThread = new Thread(Do);
+            newThread.Start();
+        }
+
+        Console.WriteLine(Singleton.Counter);
+    }
+
+    public static void Do()
+    {
+        var single = Singleton.GetInstance();
+    }
+}
---
+++ 6.1Homework/6.1Homework/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _6._1Homework;
+
+public class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton()
+    {
+
+    }
+
+    private static object _lock = new object();
+
+    public static int Counter = 0;
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+            Console.WriteLine("new obj");
+            Counter++;
+        }
+
+        return instance;
+    }
+}
